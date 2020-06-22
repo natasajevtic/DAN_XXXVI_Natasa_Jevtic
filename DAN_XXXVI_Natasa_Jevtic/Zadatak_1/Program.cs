3a40e9f9253525ad919537aeefd99f93444191b2
natasajevtic <nataly.jevtic.95@gmail.com>
2020-06-22 15:11:14 +0200

    Implemented methods for creating array of odd numbers from matrix, writing to file and reading

--- DAN_XXXVI_Natasa_Jevtic/Zadatak_1/Program.cs
+++ DAN_XXXVI_Natasa_Jevtic/Zadatak_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 
 namespace Zadatak_1
@@ -11,6 +12,7 @@ namespace Zadatak_1
         static int[,] matrix;
         static int[] randomNumbers;
         static object locker = new object();
+        static int[] oddNumbers;
         /// <summary>
         /// This method initializes matrix, and after another method generated random numbers, this method perform filling created matrix with that numbers.
         /// </summary>
@@ -22,7 +24,7 @@ namespace Zadatak_1
                 //initializing matrix
                 matrix = new int[100, 100];
                 //stopping current thread to another thread can generate random numbers
-                if (randomNumbers == null)
+                while (randomNumbers == null)
                 {
                     Monitor.Wait(locker);
                 }
@@ -61,6 +63,76 @@ namespace Zadatak_1
             }
         }
         /// <summary>
+        /// This method every odd number from matrix places in array and then in file. After that signalizes another thread that array is created and  writed in file.
+        /// </summary>
+        static void CreateArrayOfOddNumbers()
+        {
+            //locking block of code, that only one thread can access this object in the same time  
+            lock (locker)
+            {
+                int countOfOddNumbers = 0;
+                //calculating how much matrix has odd numbers
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        if (matrix[i, j] % 2 != 0)
+                        {
+                            countOfOddNumbers++;
+                        }
+                    }
+                }
+                //initializing array of odd numbers
+                oddNumbers = new int[countOfOddNumbers];
+                int k = 0;
+                //placing odd numbers from matrix in array of odd numbers
+                while (k < countOfOddNumbers)
+                {
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < matrix.GetLength(1); j++)
+                        {
+                            if (matrix[i, j] % 2 != 0)
+                            {
+                                oddNumbers[k] = matrix[i, j];
+                                k++;
+                            }
+                        }
+                    }
+                }
+                //writing created array of numbers in txt
+                StreamWriter str = new StreamWriter(@"../../Array.txt");
+                foreach (int number in oddNumbers)
+                {
+                    str.WriteLine(number);
+                }
+                str.Close();
+                //sending another thread signal that creating array of odd numbers is done
+                Monitor.Pulse(locker);
+            }
+        }
+        /// <summary>
+        /// This method waits to another tread to create and write array of odd numbers in txt, and after that displays numbers from file to console.
+        /// </summary>
+        static void PrintOddNumbers()
+        {
+            //locking block of code, that only one thread can access this object in the same time
+            lock (locker)
+            {
+                //while array is not created, waiting another thread to do that
+                while (oddNumbers == null)
+                {
+                    Monitor.Wait(locker);
+                }
+                //reading numbers from file, and displaying them to console
+                string[] numbers = File.ReadAllLines(@"../../Array.txt");
+                foreach (string number in numbers)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+        /// <summary>
         /// This method manages performing of threads.
         /// </summary>
         /// <param name="args"></param>
@@ -70,6 +142,12 @@ namespace Zadatak_1
             creatingMatrix.Start();
             Thread generateNumbers = new Thread(GenerateNumbers);
             generateNumbers.Start();
+            //blocking other threads while this two threads do not end their job
+            Thread oddNumbers = new Thread(CreateArrayOfOddNumbers);
+            Thread printer = new Thread(PrintOddNumbers);
+            oddNumbers.Start();
+            printer.Start();
+            Console.ReadLine();
         }
     }
 }
